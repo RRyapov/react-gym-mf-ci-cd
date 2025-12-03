ceedbb77319f6e3841160d4da307c62538a85f2c
Ruslan Ryapov <Ryapov@SoftLab.ru>
2025-12-03 21:21:23 +0300

    ci_cd: доработан конфиг линтера в части node_modules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,24 @@
 module.exports = {
-  root: true,
-  extends: ["@rshbintech.rprul.ecp/eslint-config/fsd"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: true,
-  },
-  ignorePatterns: [
-    "build",
-    ".eslintrc.js",
-  ],
-  rules: {
-    "prettier/prettier": [
-      "error",
-      {
-        endOfLine: "auto",
-      },
-    ],
-    "@typescript-eslint/consistent-type-definitions": "off",
-  },
+	root: true,
+	extends: ["@rshbintech.rprul.ecp/eslint-config/fsd"],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		project: true,
+	},
+	ignorePatterns: [
+		"build",
+		"node_modules",
+		".eslintrc.js",
+		"*.config.js",
+		"*.config.ts",
+	],
+	rules: {
+		"prettier/prettier": [
+			"error",
+			{
+				endOfLine: "auto",
+			},
+		],
+		"@typescript-eslint/consistent-type-definitions": "off",
+	},
 };
